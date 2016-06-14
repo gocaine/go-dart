4ffecf8219c53f0bf7b24dc1b469340c0b63cd17
Guillaume GERBAUD <ggerbaud@mabreizh.fr>
2016-06-14 21:30:38 +0200

    add controls for x01 games

--- server/x01.go
+++ server/x01.go
@@ -6,13 +6,14 @@ type Gamex01 struct {
 	score   int
 	started bool
 	players []string
+	remains []int
 }
 
-func NewGame(score int) *Gamex01 {
+func NewGamex01(score int) *Gamex01 {
 	g := new(Gamex01)
 
 	g.score = score
-	g.players = make([]string, 4)
+	g.players = make([]string, 0, 4)
 
 	return g
 }
@@ -26,8 +27,12 @@ func (game *Gamex01) AddPlayer(name string) {
 }
 
 func (game *Gamex01) Start() {
-	if !game.started {
+	if !game.started && len(game.players) > 0 {
 		game.started = true
+		game.remains = make([]int, len(game.players))
+		for i := range game.remains {
+			game.remains[i] = game.score
+		}
 	} else {
 		panic("Game already started")
 	}
